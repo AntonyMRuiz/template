ac230b9bc4a4d6aabaab5a1fa0e613f92b865eaa
antony martinez <antonymaruiz02@gmail.com>
2024-09-15 00:57:29 -0500

    open all

--- src/main/java/com/riwi/template/infrastructure/config/OpenApiConfiguration.java
+++ src/main/java/com/riwi/template/infrastructure/config/OpenApiConfiguration.java
@@ -2,9 +2,9 @@ package com.riwi.template.infrastructure.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,8 +13,11 @@ import org.springframework.context.annotation.Configuration;
 @OpenAPIDefinition(info = @Info(
         title = "Template",
         version = "1.0",
-        description = "Usage template spring security, mapStructure, JWT"
-))
+        description = "Usage template spring security, mapStructure, JWT"),
+        servers = {
+                @Server(url = "https://template.up.railway.app/api/v1", description = "Railway Server")
+        }
+)
 public class OpenApiConfiguration {
 
     @Bean
--- src/main/java/com/riwi/template/infrastructure/config/WebConfig.java
+++ src/main/java/com/riwi/template/infrastructure/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.riwi.template.infrastructure.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,17 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
-    @Value("${cors.allowed}")
-    private String corsAllowed;
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(corsAllowed.split(","))
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
